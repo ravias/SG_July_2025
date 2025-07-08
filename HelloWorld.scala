f17ab5a8f8e2bdfa59663a0521a3f685f4bb8f4b
ravias <ravias@users.noreply.github.com>
2025-07-08 15:16:58 +0530

    Add files via upload

---
+++ HelloWorld.scala
@@ -0,0 +1,12 @@
+// Comments in a Scala program
+// Scala supports single-line and multi-line comments very similar to Java. 
+// Scala program file. Compiling and running
+
+object HelloWorld { 
+/* This is my first program.
+ * This will print 'Hello World' as the output
+*/
+    def main(args: Array[String]) {
+        println("Hello, world!") // prints Hello World
+    }
+}
